8c6ce6f67f8c6e4e48047ae3ec7ed7f7464544e9
eff-kay <faizan10114@gmail.com>
2025-01-27 15:07:37 -0500

    fix build

--- next.config.js
+++ next.config.js
@@ -1,12 +1,29 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: "export",
+//     assetPrefix: "/code-fix-bench/",
+//     basePath: "/code-fix-bench",
+//     images: {
+//       unoptimized: true,
+//     },
+//   }
+  
+//   module.exports = nextConfig
+  
+
+// const { withContentlayer } = require("next-contentlayer");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    assetPrefix: "/code-fix-bench/",
-    basePath: "/code-fix-bench",
-    images: {
-      unoptimized: true,
-    },
-  }
-  
-  module.exports = nextConfig
-  
+  reactStrictMode: true,
+  swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: { unoptimized: true },
+  distDir: "build",
+  output: "export",
+};
+
+// module.exports = withContentlayer(nextConfig);
+module.exports = nextConfig;
